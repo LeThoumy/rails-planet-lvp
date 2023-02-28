ad2954c498ac16833707ddf4729b8087f7dfd53b
thomas stamour <thomastamour@gmail.com>
2023-02-28 17:27:43 -0500

    Added hero & advantages section

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "planets#index"
 
-
   # get "/", to: "pages"
   # get "/planets", to: "planets#index"
   # get "/planets/:id", to:'planets#show'
