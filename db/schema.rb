661c1882c3845f7139bc49d8d2c1ef1f27f106fc
hiyabel belai <hiyabel13@gmail.com>
2023-03-03 14:00:44 -0500

    before merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_002655) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_185129) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,13 +44,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_002655) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer "total_price"
-    t.string "status"
-    t.integer "start_date"
-    t.integer "end_date"
+    t.string "status", default: "pending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.bigint "planet_id", null: false
+    t.datetime "start_date"
+    t.datetime "end_date"
     t.index ["planet_id"], name: "index_bookings_on_planet_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
