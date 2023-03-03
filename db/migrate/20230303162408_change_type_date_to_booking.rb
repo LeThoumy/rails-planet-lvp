2d1bff74387ffbbd062c0b998c2501cfc5709823
hiyabel belai <hiyabel13@gmail.com>
2023-03-03 12:01:16 -0500

    finishedthebooking

---
+++ db/migrate/20230303162408_change_type_date_to_booking.rb
@@ -0,0 +1,8 @@
+class ChangeTypeDateToBooking < ActiveRecord::Migration[7.0]
+  def change
+    remove_column :bookings, :start_date, :integer
+    remove_column :bookings, :end_date, :integer
+    add_column :bookings, :start_date, :datetime
+    add_column :bookings, :end_date, :datetime
+  end
+end
